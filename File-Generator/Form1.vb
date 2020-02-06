146f30e67eb2794b129dbabb94da4f1444489ff0
guintomarianne <guintomarianne@gmail.com>
2020-02-06 18:28:32 +0800

    Fourth Commit - XML File Generation

--- File-Generator/Form1.vb
+++ File-Generator/Form1.vb
@@ -1,4 +1,5 @@
-﻿Imports System.IO
+﻿Imports System
+Imports System.IO
 Imports System.Xml
 
 
@@ -16,7 +17,7 @@ Public Class Form1
             sw1.WriteLine("Age: " & AgeTextBox.Text)
             sw1.WriteLine("Address: " & AddressTextBox.Text)
 
-            MsgBox("File successfully created!", MsgBoxStyle.Information, "Saved")
+            MsgBox("TXT File successfully created!", MsgBoxStyle.Information, "Saved")
             clearboxes()
 
             sw1.Close()
@@ -24,11 +25,47 @@ Public Class Form1
         Else
             MsgBox("Please input data into boxes", MsgBoxStyle.Critical, "Complete the data")
         End If
-
     End Sub
 
 
 
+    Private Sub XMLButton_Click(sender As Object, e As EventArgs) Handles XMLButton.Click
+        If NameTextBox.Text <> "" Or AgeTextBox.Text <> "" Or AddressTextBox.Text <> "" Then
+
+            Dim settings As New XmlWriterSettings()
+            settings.Indent = True
+
+            Dim filename As String = "../XMLFile_" & System.DateTime.Now.ToString("MMddyyyy_hh_mm_ss") & ".xml"
+            Dim XmlWrt As XmlWriter = XmlWriter.Create(filename, settings)
+
+            With XmlWrt
+                .WriteStartDocument()
+                .WriteComment("XML File")
+                .WriteStartElement("Personal-Data")
+                .WriteStartElement("Name")
+                .WriteString(NameTextBox.Text.ToString())
+                .WriteEndElement()
+
+                .WriteStartElement("Age")
+                .WriteString(AgeTextBox.Text.ToString())
+                .WriteEndElement()
+
+                .WriteStartElement("Address")
+                .WriteString(AddressTextBox.Text.ToString())
+                .WriteEndElement()
+
+                .WriteEndElement()
+                .WriteEndDocument()
+                .Close()
+            End With
+
+            MsgBox("XML File successfully created!", MsgBoxStyle.Information, "Saved")
+            clearboxes()
+
+        Else
+            MsgBox("Please input data into boxes", MsgBoxStyle.Critical, "Complete the data")
+        End If
+    End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
